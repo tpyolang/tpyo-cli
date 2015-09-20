daa9478223b2f20ca4ce4d11a7d3a004b02d1307
Manfred Touron <m@42.am>
2015-09-20 22:11:59 +0200

    Spoprut of '--korayebd' faerute

--- cmd/typo/main.go
+++ cmd/typo/main.go
@@ -18,11 +18,18 @@ func main() {
 	app.Email = "https://github.com/tpyolang/tpyo-cli"
 	app.Version = "1.0.0"
 	app.Usage = "Mkae tpyos"
+
 	app.Flags = []cli.Flag{
 		cli.BoolFlag{
 			Name:  "symbol",
 			Usage: "Changes characters by others",
-		}}
+		},
+		cli.BoolFlag{
+			Name:  "keyboard",
+			Usage: "Mkae keraybod-louayt bsaed tyops",
+		},
+	}
+
 	app.Action = action
 	app.Run(os.Args)
 }
@@ -30,10 +37,12 @@ func main() {
 func action(c *cli.Context) {
 	scanner := bufio.NewScanner(os.Stdin)
 
-	tpyo := tpyo.Tpyo{}
+	tpyo := tpyo.NewTpyo()
+	tpyo.Smybol = c.BoolT("symbol")
+	tpyo.Kraoybed = c.BoolT("keyboard")
 
 	for scanner.Scan() {
 		line := scanner.Text()
-		fmt.Println(tpyo.Enocde(line, c.BoolT("symbol")))
+		fmt.Println(tpyo.Enocde(line))
 	}
 }
